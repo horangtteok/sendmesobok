dee7bf26dc1275cc99f36c875e8a3516c300ae81
kimsoyeong <iloveit356@gmail.com>
2022-01-02 04:48:40 +0900

    change backend port to 8080
    - from 5000 to 8080

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+const cors = require('cors');
+
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 
@@ -19,13 +21,15 @@ app.use(bodyParser.json());
 // cookie-parser option
 app.use(cookieParser());
 
+app.use(cors());
+
 app.use('/api/users', require('./routes/users'));
 app.use('/api/posts', require('./routes/posts'));
 
 app.get('/', (req, res) => res.send('<h1>Hello World! 반갑습니다!</h1>'));
 
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}!`)

b868a9af78fedc890a22732b9dda6713c39b295c
Soyeong Kim <43427380+kimsoyeong@users.noreply.github.com>
2022-01-08 23:40:19 +0900

    Update Config.js

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -1,3 +1,4 @@
 //SERVER ROUTES
 export const USER_SERVER = 'https://sendmesobok.herokuapp.com/api/users';
 export const POST_SERVER = 'https://sendmesobok.herokuapp.com/api/posts';
+export const KAKAO_KEY = `${process.env.KAKAO_KEY}`;

70f341aa5624f570757567cac89d6cc932d09f59
kimsoyeong <iloveit356@gmail.com>
2022-01-10 06:30:50 +0900

    convert gif to mp4

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -6,8 +6,14 @@ function LandingPage( props ) {
 
     return (
         <>
-        
-            <div className="app" style={{ backgroundImage: "url('img/back-repeat-red2.gif')" }}>             
+            <div className="app">
+                <div class="video-wrapper">
+                    <video autoPlay muted id="back-video">
+                        <source src="https://user-images.githubusercontent.com/43427380/148701456-dba9e7ce-afb3-4ad4-834a-c12574ad6403.mp4
+" type="video/mp4" />
+                        <strong>vieo tag를 지원하지 않는 브라우저입니다.</strong>
+                    </video>
+                </div>            
                 {user.userData && user.userData.isAuth  ?
                     <a className="btn_start" href={`/post/${user.userData._id}`}>복주머니로 이동</a>
                     : 
